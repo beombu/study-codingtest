b228cf24b4a5d08f38029c1e40b49037f1f440da
beombu <sunil1369@bible.ac.kr>
2022-11-27 17:04:14 +0900

    Feat(boj) : 구간 합 구하기 5 문제 코드

---
+++ src/site/boj/arrAndList/구간_합_구하기_5_11660.java
@@ -0,0 +1,42 @@
+package site.boj.arrAndList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 구간_합_구하기_5_11660 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] sumArr = new int[N + 1][N + 1];
+        for(int i = 1; i < sumArr.length; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 1; j < sumArr[0].length; j++){
+                sumArr[i][j] = sumArr[i][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int a = 0; a < M; a++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int sum = 0;
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            for(int i = x1; i <= x2; i++){
+                sum += sumArr[i][y2] - sumArr[i][y1 -1];
+            }
+            sb.append(sum + "\n");
+        }
+        System.out.print(sb);
+    }
+}
