32b69a2da372bb5e4d25ba47f21655b0834b651a
beombu <sunil1369@bible.ac.kr>
2022-11-28 13:06:50 +0900

    feat(boj) : (시간 초과) 나머지 합 문제 풀이

---
+++ src/site/boj/arrAndList/나머지_합_10986.java
@@ -0,0 +1,37 @@
+package site.boj.arrAndList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 나머지_합_10986 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            int sum1 = arr[i];
+            for(int j = 1; j < arr.length; j++){
+                sum1 += arr[j];
+                if(sum1 % M == 0){
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
