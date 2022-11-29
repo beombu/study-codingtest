994f7c0e2a2ec84c96bd1ed2c17c89b3d02328e9
beombu <sunil1369@bible.ac.kr>
2022-11-29 19:42:58 +0900

    Feat(boj) : (실패)좋다 - 투 포인터 문제

---
+++ src/site/boj/arrAndList/좋다_1253.java
@@ -0,0 +1,47 @@
+package site.boj.arrAndList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 좋다_1253 {
+    public static int N;
+    public static int[] arr;
+    public static int cnt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for(int k = 0; k < N; k++){
+            int find = arr[k];
+            int l = 0;
+            int r = N - 1;
+            while(l < r){
+                int sum = arr[l] + arr[r];
+
+                if(sum == find){
+                    cnt++;
+                }
+
+                if(sum > find){
+                    r--;
+                }else if(sum <= find){
+                    l++;
+                }
+            }
+        }
+    }
+}
