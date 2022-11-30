2ac2eccc4b13ee74d5bca996ca2e46677e38e684
beombu <sunil1369@bible.ac.kr>
2022-11-30 20:07:26 +0900

    Feat(inflearn) : 중복문자 제거

---
+++ src/site/inflearn/String/중복문자제거.java
@@ -0,0 +1,22 @@
+package site.inflearn.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 중복문자제거 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        String result = "";
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.indexOf(str.charAt(i)) == i){
+                result += str.charAt(i);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
