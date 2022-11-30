fcd223154033f240d1996e41087c0a9358547093
beombu <sunil1369@bible.ac.kr>
2022-11-30 18:54:40 +0900

    Feat(inflearn) : 단어 뒤집기

---
+++ src/site/inflearn/String/단어_뒤집기.java
@@ -0,0 +1,24 @@
+package site.inflearn.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 단어_뒤집기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            String str = br.readLine();
+            StringBuilder sb = new StringBuilder();
+
+            for(int j = str.length() -1; j >= 0; j--){
+                sb.append(str.charAt(j));
+            }
+
+            System.out.println(sb);
+        }
+    }
+}
