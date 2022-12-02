4f10ef2b9b79b339983d1453bc0f912f8fcae152
beombu <sunil1369@bible.ac.kr>
2022-12-02 14:32:58 +0900

    Feat(inflearn) : 유효한 팰린드롬 - isAlphabetic

---
+++ src/site/inflearn/String/유효한_팰린드롬.java
@@ -0,0 +1,27 @@
+package site.inflearn.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 유효한_팰린드롬 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String newStr = "";
+        String result = "YES";
+
+        for(int i = 0; i < str.length(); i++){
+            if(Character.isAlphabetic(str.charAt(i))){
+                newStr += str.charAt(i);
+            }
+        }
+
+        String reverseStr = new StringBuilder(newStr).reverse().toString();
+        if(!newStr.equalsIgnoreCase(reverseStr)){
+            result = "NO";
+        }
+
+        System.out.println(result);
+    }
+}
