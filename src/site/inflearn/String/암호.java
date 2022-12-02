2b00f5c2be1e7d06877873946c9195ff27ba48b8
beombu <sunil1369@bible.ac.kr>
2022-12-02 17:41:09 +0900

    Feat(inflearn) : 암호 - 이진수 변환, 아스키 코드 변환

---
+++ src/site/inflearn/String/암호.java
@@ -0,0 +1,42 @@
+package site.inflearn.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 암호 {
+    public static final int PASSWORD_LENGTH = 7;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String[] strArr = new String[N];
+        int cnt = 0;
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '#'){
+                sb.append(1);
+            }else if(str.charAt(i) == '*'){
+                sb.append(0);
+            }
+            if((i + 1) % PASSWORD_LENGTH == 0){
+                strArr[cnt] = sb.toString();
+                sb.setLength(0);
+                cnt++;
+            }
+        }
+
+        sb = new StringBuilder();
+
+        for(String s : strArr){
+            int num = Integer.valueOf(s, 2);
+            char ch = (char) num;
+            sb.append(ch);
+        }
+
+        System.out.println(sb);
+    }
+}
