07e7362326f076b45ffcf4891efe388b0464dac8
beombu <sunil1369@bible.ac.kr>
2023-01-06 19:13:43 +0900

    Feat(inflearn) : 순열 구하기 - 백트래킹으로 ch해제

---
+++ src/site/inflearn/DFS_BFS/순열_구하기.java
@@ -0,0 +1,47 @@
+package site.inflearn.DFS_BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 순열_구하기 {
+    static int M;
+    static int[] arr, pm, ch;
+
+    public static void DFS(int L) {
+        if (L == M) {
+            for (int a : pm) {
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    pm[L] = arr[i];
+                    DFS(L + 1);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        ch = new int[N];
+        pm = new int[M];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        DFS(0);
+    }
+}
