e0d90657835aaa8b3914c0a36cf5a220e83a71a5
beombu <sunil1369@bible.ac.kr>
2023-01-23 20:00:27 +0900

    Feat(boj) : 스택 수열 - 스택 + sb

---
+++ src/site/boj/문제집/코딩테스트준비/연습/알고리즘_기초/스택_수열.java
@@ -0,0 +1,39 @@
+package site.boj.문제집.코딩테스트준비.연습.알고리즘_기초;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class 스택_수열 {
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        int start = 0;
+
+        while (n-- > 0) {
+            int value = Integer.parseInt(br.readLine());
+
+            if (value > start) {
+                for (int i = start + 1; i <= value; i++) {
+                    stack.push(i);
+                    sb.append('+').append("\n");
+                }
+                start = value;
+            } else if (stack.peek() != value) {
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append('-').append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
