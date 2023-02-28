c67a314098cd3b4567ac8f1f1af7341a40fdf008
beombu <sunil1369@bible.ac.kr>
2023-02-28 17:00:38 +0900

    Refactor(programmers) : 주식 가격 - stack 삭제

--- src/site/programmers/codingKit/stack_queue/주식가격.java
+++ src/site/programmers/codingKit/stack_queue/주식가격.java
@@ -1,27 +1,16 @@
 package site.programmers.codingKit.stack_queue;
 
-import java.util.Stack;
-
 public class 주식가격 {
     public static int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> beforePrices = new Stack<>();//가격이 크거나 같으면 들어가는 스택
 
         for (int i = 0; i < prices.length; i++) {
             int nowPrice = prices[i];
 
             for (int j = i + 1; j < prices.length; j++) {
-                if(j == prices.length - 1){//마지막 index라면
-                    answer[i] = beforePrices.size() + 1;
-                    beforePrices.clear();
-                    break;
-                }
-                //가격이 크거나 같으면
-                if (nowPrice <= prices[j]) {
-                    beforePrices.push(prices[j]);
-                }else {
-                    answer[i] = beforePrices.size() + 1;
-                    beforePrices.clear();
+                answer[i]++;
+
+                if(nowPrice > prices[j]){
                     break;
                 }
             }
