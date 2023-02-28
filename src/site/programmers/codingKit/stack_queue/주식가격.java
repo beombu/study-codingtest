41edeb45e41e9f6d6e86d78bf225ff3994e57abe
beombu <sunil1369@bible.ac.kr>
2023-02-28 16:32:15 +0900

    Fix(programmers) : 주식가격 - stack으로 변환했지만 stack이 의미가 없는...

--- src/site/programmers/codingKit/stack_queue/주식가격.java
+++ src/site/programmers/codingKit/stack_queue/주식가격.java
@@ -1,36 +1,31 @@
 package site.programmers.codingKit.stack_queue;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Stack;
 
 public class 주식가격 {
     public static int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Queue<Integer> beforePrices = new LinkedList<>();//진행했던 가격들
+        Stack<Integer> beforePrices = new Stack<>();//가격이 크거나 같으면 들어가는 스택
 
         for (int i = 0; i < prices.length; i++) {
             int nowPrice = prices[i];
 
-            //진행했던 가격들 반복
-            for (int j = 0; j < beforePrices.size(); j++) {
-                //가격이 크거나 같으면
-                if(nowPrice >= beforePrices.peek()){
-                    answer[j]++;
+            for (int j = i + 1; j < prices.length; j++) {
+                if(j == prices.length - 1){//마지막 index라면
+                    answer[i] = beforePrices.size() + 1;
+                    beforePrices.clear();
+                    break;
                 }
-                beforePrices.offer(beforePrices.poll());
+                //가격이 크거나 같으면
+                if (nowPrice <= prices[j]) {
+                    beforePrices.push(prices[j]);
+                }else {
+                    answer[i] = beforePrices.size() + 1;
+                    beforePrices.clear();
+                    break;
+                }
             }
-
-            beforePrices.offer(nowPrice);//가격 큐에 삽입
         }
-
         return answer;
-    }
-
-    public static void main(String[] args) {
-        int[] solution = solution(new int[]{3, 2, 3, 2, 3});
-
-        for(int i : solution){
-            System.out.print(i + " ");
-        }
     }
 }
