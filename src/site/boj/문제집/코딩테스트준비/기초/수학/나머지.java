f142f09f96f03afeb5538931e4c27fb595b61e92
beombu <beombu13@gmail.com>
2023-05-25 21:36:12 +0900

    Feat(Boj) : 나머지

---
+++ src/site/boj/문제집/코딩테스트준비/기초/수학/나머지.java
@@ -0,0 +1,21 @@
+package site.boj.문제집.코딩테스트준비.기초.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 나머지 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+
+        int A = Integer.parseInt(arr[0]);
+        int B = Integer.parseInt(arr[1]);
+        int C = Integer.parseInt(arr[2]);
+
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.print(((A % C) * (B % C)) % C);
+    }
+}
