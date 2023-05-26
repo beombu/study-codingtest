b9e3eaf5e2348fe197ff588d82e58d6119c6ec50
beombu <beombu13@gmail.com>
2023-05-26 22:11:49 +0900

    Refactor(boj) : 약수의 합2
    배수를 이용 (N / i) * i

--- src/site/boj/문제집/코딩테스트준비/기초/수학/약수의_합2.java
+++ src/site/boj/문제집/코딩테스트준비/기초/수학/약수의_합2.java
@@ -8,27 +8,21 @@ public class 약수의_합2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int answer = 0;
+        long answer = 0;
 
+        /**
+         * N = 6
+         * i = 1 -> (6 / 1) * 1
+         * i = 2 -> (6 / 2) * 2
+         * i = 3 -> (6 / 3) * 3
+         * i = 4 -> (6 / 4) * 4
+         * i = 5 -> (6 / 5) * 5
+         * i = 6 -> (6 / 6) * 6
+         */
         for (int i = 1; i <= N; i++) {
-            answer += sumDivisor(i);
+            answer += (N / i) * i;
         }
 
         System.out.println(answer);
-    }
-
-    private static int sumDivisor(int N) {
-        int sum = 0;
-        for (int i = 1; i <= (int) Math.sqrt(N); i++) {
-            if (N % i == 0) {
-                sum += i;
-
-                if ((N / i) != i) {
-                    sum += (N / i);
-                }
-            }
-        }
-
-        return sum;
     }
 }
