f08caefa36772d159e731bc7bc9080222182e1ca
beombu <beombu13@gmail.com>
2023-12-15 09:29:31 +0900

    Feat(boj) : N과 M(5)-DFS

---
+++ src/site/boj/문제집/코딩테스트준비/기초/N과M/N과M_5.java
@@ -0,0 +1,56 @@
+package site.boj.문제집.코딩테스트준비.기초.N과M;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N과M_5 {
+	public static int N;
+	public static int M;
+	public static int[] arr;
+	public static int[] printArr;
+	public static boolean[] visited;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		printArr = new int[M];
+		visited = new boolean[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		dfs(0);
+		System.out.println(sb);
+	}
+
+	public static void dfs(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(printArr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				printArr[depth] = arr[i];
+				dfs(depth + 1);
+				visited[i] = false;
+			}
+		}
+	}
+}
