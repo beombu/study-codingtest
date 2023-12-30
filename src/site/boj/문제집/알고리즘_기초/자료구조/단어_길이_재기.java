8a3397cbce3fa8013ada0e0f56599db0d5da991c
beombu <beombu13@gmail.com>
2023-12-30 23:12:13 +0900

    Feat(boj) : 단어 길이 재기

---
+++ src/site/boj/문제집/알고리즘_기초/자료구조/단어_길이_재기.java
@@ -0,0 +1,14 @@
+package site.boj.문제집.알고리즘_기초.자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 단어_길이_재기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+
+		System.out.println(S.length());
+	}
+}
